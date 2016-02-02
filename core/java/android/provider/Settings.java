0ed72bcabe78a61edc97f8b24969b9a0ff7b5c12
d34d <clark@cyngn.com>
2016-02-02 15:47:56 -0800

    Battery: Show battery fully charged notification [1/2]
    @mydongistiny: Added notification channel parts so the notification
    actually shows on 8.0+ and added a toggle instead of a config option
    to enable or disable.
    
    Change-Id: I14caaf9914e499afeb15fe177630e6ecb06bd106
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Reza Adi Pangestu <rezaadipangestu5@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4865,6 +4865,12 @@ public final class Settings {
         public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
 
         /**
+         * Whether to notification when the battery is fully charged
+         * @hide
+         */
+        public static final String BATTERY_FULLY_CHARGED_NOTIF = "battery_fully_charged_notif";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
