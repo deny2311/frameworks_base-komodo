a3fbbc754c4af9e10f91a6151780cc739fe06398
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    [1/2] base: HeadsUp add timeout option
    Forward ported to marshmallow/nougat By: BeansTown106
    this was basically a rewrite as everything about it changed
    
    Forward ported to 10 by eyosen
    
    Change-Id: Ia8afb6494a3feb1f4f2ff6c13e8b6444c38a7506

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5293,6 +5293,16 @@ public final class Settings {
         public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
 
         /**
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /** @hide */
+        private static final Validator HEADS_UP_TIMEOUT_VALIDATOR =
+              new SettingsValidators.InclusiveIntegerRangeValidator(0, 10000);
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -5403,6 +5413,7 @@ public final class Settings {
             FOOTER_TEXT_STRING,
             FINGERPRINT_SUCCESS_VIB,
             DOUBLE_TAP_SLEEP_GESTURE,
+            HEADS_UP_TIMEOUT,
             // komodo
         };
 
@@ -5585,6 +5596,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(FINGERPRINT_SUCCESS_VIB);
             PRIVATE_SETTINGS.add(DISPLAY_CUTOUT_HIDDEN);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
+            PRIVATE_SETTINGS.add(HEADS_UP_TIMEOUT);
             // Komodo panels end
         }
 
@@ -5737,6 +5749,7 @@ public final class Settings {
             VALIDATORS.put(FINGERPRINT_SUCCESS_VIB, FINGERPRINT_SUCCESS_VIB_VALIDATOR);
             VALIDATORS.put(DISPLAY_CUTOUT_HIDDEN, DISPLAY_CUTOUT_HIDDEN_VALIDATOR);
             VALIDATORS.put(DOUBLE_TAP_SLEEP_GESTURE, DOUBLE_TAP_SLEEP_GESTURE_VALIDATOR);
+            VALIDATORS.put(HEADS_UP_TIMEOUT,HEADS_UP_TIMEOUT_VALIDATOR);
             // Komodo panels end
         }
 
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpManager.java
@@ -64,7 +64,11 @@ public abstract class HeadsUpManager extends AlertingNotificationManager {
         mAccessibilityMgr = Dependency.get(AccessibilityManagerWrapper.class);
         Resources resources = context.getResources();
         mMinimumDisplayTime = resources.getInteger(R.integer.heads_up_notification_minimum_time);
-        mAutoDismissNotificationDecay = resources.getInteger(R.integer.heads_up_notification_decay);
+        int defaultHeadsUpNotificationDecayMs =
+                resources.getInteger(R.integer.heads_up_notification_decay);
+        mAutoDismissNotificationDecay = Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.HEADS_UP_TIMEOUT,
+                defaultHeadsUpNotificationDecayMs, UserHandle.USER_CURRENT);
         mTouchAcceptanceDelay = resources.getInteger(R.integer.touch_acceptance_delay);
         mSnoozedPackages = new ArrayMap<>();
         int defaultSnoozeLengthMs =
@@ -367,6 +371,15 @@ public abstract class HeadsUpManager extends AlertingNotificationManager {
     protected class HeadsUpEntry extends AlertEntry {
         public boolean remoteInputActive;
         protected boolean expanded;
+
+        @Override
+        public void updateEntry(boolean updatePostTime) {
+            mAutoDismissNotificationDecay = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.HEADS_UP_TIMEOUT,
+                mContext.getResources().getInteger(R.integer.heads_up_notification_decay),
+                UserHandle.USER_CURRENT);
+            super.updateEntry(updatePostTime);
+        }
 
         @Override
         protected boolean isSticky() {
