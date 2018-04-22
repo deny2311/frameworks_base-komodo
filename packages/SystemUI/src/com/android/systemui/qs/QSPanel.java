89863cca3475e8247269921631d4c045705157a8
PMS22 <pratham220399@gmail.com>
2018-04-22 16:51:08 +0000

    Brightness slider: Vibrate after a long press
    - A small haptic feedback after long pressing feels good
    
    Change-Id: I1058bbcf24bbccaeeebb3f547905860552bd1131

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -30,6 +30,8 @@ import android.metrics.LogMaker;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.service.quicksettings.Tile;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -106,6 +108,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
     private int mBrightnessSlider = 1;
 
+    private final Vibrator mVibrator;
+
     public QSPanel(Context context) {
         this(context, null);
     }
@@ -122,6 +126,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
         final ContentResolver resolver = context.getContentResolver();
 
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
         setOrientation(VERTICAL);
 
         mBrightnessView = LayoutInflater.from(mContext).inflate(
@@ -160,6 +165,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             @Override
             public boolean onLongClick(View v) {
                 setBrightnessMin(context, isAdaptiveBrightness(context));
+                mVibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
                 return false;
             }
         });
@@ -192,6 +198,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             @Override
             public boolean onLongClick(View v) {
                 setBrightnessMax(context, isAdaptiveBrightness(context));
+                mVibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
                 return false;
             }
         });
