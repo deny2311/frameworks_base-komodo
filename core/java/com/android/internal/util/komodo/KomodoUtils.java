ec8a34f19ebc776af47be8a9d375a12226cc343b
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 11:10:54 -0600

    Method to detect a notch'd device
    Change-Id: I4742eaeea94c4495361bdfe2b855ae0de0aa24c1

--- core/java/com/android/internal/util/komodo/KomodoUtils.java
+++ core/java/com/android/internal/util/komodo/KomodoUtils.java
@@ -36,6 +36,7 @@ import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
+import android.util.DisplayMetrics;
 
 import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
@@ -174,5 +175,24 @@ public class KomodoUtils {
                 }
             }
         }
+    }
+
+    //Method to detect a notch'd device
+    public static boolean hasNotch(Context context) {
+        int result = 0;
+        int resid;
+        int resourceId = context.getResources().getIdentifier(
+                "status_bar_height", "dimen", "android");
+        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
+                "bool", "android");
+        if (resid > 0) {
+            return context.getResources().getBoolean(resid);
+        }
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = 24 * (metrics.densityDpi / 160f);
+        return result > Math.round(px);
     }
 }
