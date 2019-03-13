b2d537ebcf4835bf998b6a5ffb820e48c0edc5cf
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-03-13 16:51:24 -0400

    NetworkTraffic: Make the indicator arrows dynamic
    * display arrow indicators accordingly for the traffic data
      that is being shown.
    
      - UP arrow indicator for transmit data speed
          https://imgur.com/6bZOWvH
    
      - DOWN arrow indicator for receive data speed
          https://imgur.com/hIMHBN1
    
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>
    Change-Id: Iec49abc1d1d81fcd9c9464e03e789cb8ff805c13
    Signed-off-by: Kry9toN <dhimasbagusprayoga@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -69,12 +69,15 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
     private long lastUpdateTime;
     private int txtSize;
     private boolean mHideArrow;
+    private int txtImgPadding;
     private int mAutoHideThreshold;
     private int mTintColor;
     private int mVisibleState = -1;
     private boolean mTrafficVisible = false;
     private boolean mSystemIconVisible = true;
     private boolean mColorIsStatic = false;
+    private boolean indicatorUp = false;
+    private boolean indicatorDown = false;
 
     private Handler mTrafficHandler = new Handler() {
         @Override
@@ -111,6 +114,7 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
                     setTextSize(TypedValue.COMPLEX_UNIT_PX, (float)txtSize);
                     setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
                     setText(output);
+                    indicatorUp = true;
                 }
                 mTrafficVisible = true;
             } else {
@@ -123,10 +127,12 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
                     setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
 		            setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
                     setText(output);
+                    indicatorDown = true;
                 }
                 mTrafficVisible = true;
             }
             updateVisibility();
+            updateTrafficDrawable();
 
             // Post delayed message to refresh in ~1000ms
             totalRxBytes = newTotalRxBytes;
@@ -219,6 +225,7 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
         super(context, attrs, defStyle);
         final Resources resources = getResources();
         txtSize = resources.getDimensionPixelSize(R.dimen.net_traffic_multi_text_size);
+        txtImgPadding = resources.getDimensionPixelSize(R.dimen.net_traffic_txt_img_padding);
         mTintColor = resources.getColor(android.R.color.white);
         Handler mHandler = new Handler();
         SettingsObserver settingsObserver = new SettingsObserver(mHandler);
@@ -307,16 +314,37 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
     }
 
     private void updateTrafficDrawable() {
-        int intTrafficDrawable;
+        int indicatorDrawable;
         if (mIsEnabled && !mHideArrow) {
+            if (indicatorUp) {
+                indicatorDrawable = R.drawable.stat_sys_network_traffic_up_arrow;
+                Drawable d = getContext().getDrawable(indicatorDrawable);
+                d.setColorFilter(mTintColor, Mode.MULTIPLY);
+                setCompoundDrawablePadding(txtImgPadding);
+                setCompoundDrawablesWithIntrinsicBounds(null, null, d, null);
+            } else if (indicatorDown) {
+                indicatorDrawable = R.drawable.stat_sys_network_traffic_down_arrow;
+                Drawable d = getContext().getDrawable(indicatorDrawable);
+                d.setColorFilter(mTintColor, Mode.MULTIPLY);
+                setCompoundDrawablePadding(txtImgPadding);
+                setCompoundDrawablesWithIntrinsicBounds(null, null, d, null);
+            } else {
+                setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+            }
+	}
+        } else {
             setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
         }
+        indicatorUp = false;
+        indicatorDown = false;
     }
 
     public void onDensityOrFontScaleChanged() {
         final Resources resources = getResources();
         txtSize = resources.getDimensionPixelSize(R.dimen.net_traffic_multi_text_size);
+        txtImgPadding = resources.getDimensionPixelSize(R.dimen.net_traffic_txt_img_padding);
         setTextSize(TypedValue.COMPLEX_UNIT_PX, (float)txtSize);
+        setCompoundDrawablePadding(txtImgPadding);
         setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
     }
 
