94b7f7b7edafcb0d84c4fec0113c994c6bbbdc0d
ezio84 <brabus84@gmail.com>
2019-12-26 21:19:52 -0500

    Powermenu: Give screen record it's own item [1/2]
    - Make screen record optional while we're at it.
    - Turn off screenshot by default.
    
    Credit to Google / Material Design Icons @ http://materialdesignicons.com
    for icon http://materialdesignicons.com/icon/video.
    
    Change-Id: If643417d5001f9e660d36853544f01ae1d2badd0
    
    Also Unlink screenrecord toggle from featureflags and enable it
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/facfed79042d62063fae40bc8643c1c3a3bf9700
    
    Change-Id: I11dafae0dbbdb2433b2934659be65e2f2ff4fdd8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9826,6 +9826,12 @@ public final class Settings {
          */
         public static final String LOCKDOWN_IN_POWER_MENU = "lockdown_in_power_menu";
 
+        /**
+         * Whether the Screen record button should be shown in the power menu.
+         * @hide
+         */
+        public static final String SCREENRECORD_IN_POWER_MENU = "screenrecord_in_power_menu";
+
         private static final Validator LOCKDOWN_IN_POWER_MENU_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
