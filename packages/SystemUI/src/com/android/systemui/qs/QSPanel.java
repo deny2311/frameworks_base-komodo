8d1a7abd4895978a129e196ffb5aec14b3236d86
00day0 <therandomuser11@gmail.com>
2019-12-29 20:04:41 +0700

    base: Get rid of divider in QS
    Signed-off-by: Kry9toN <dhimasbagusprayoga@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -110,7 +110,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     private Record mDetailRecord;
 
     private BrightnessMirrorController mBrightnessMirrorController;
-    private View mDivider;
 
     public QSPanel(Context context) {
         this(context, null);
@@ -162,11 +161,25 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
                 com.android.internal.R.bool.config_automatic_brightness_available);
     }
 
-    protected void addDivider() {
-        mDivider = LayoutInflater.from(mContext).inflate(R.layout.qs_divider, this, false);
-        mDivider.setBackgroundColor(Utils.applyAlpha(mDivider.getAlpha(),
-                getColorForState(mContext, Tile.STATE_ACTIVE)));
-        addView(mDivider);
+    private void addQSPanel() {
+        if (mBrightnessSlider == 1) {
+            addView(mBrightnessView);
+            addView((View) mTileLayout);
+        } else {
+            addView((View) mTileLayout);
+            addView(mBrightnessView);
+        }
+
+        addView(mFooter.getView());
+        updateResources();
+    }
+
+    private void restartQSPanel() {
+        if (mFooter.getView() != null) removeView(mFooter.getView());
+        if ((View) mTileLayout != null) removeView((View) mTileLayout);
+        if (mBrightnessView != null) removeView(mBrightnessView);
+
+        addQSPanel();
     }
 
     @Override
@@ -188,7 +201,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     }
 
     public View getDivider() {
-        return mDivider;
+        return LayoutInflater.from(mContext).inflate(R.layout.qs_divider, this, false);
     }
 
     public QSTileRevealController getQsTileRevealController() {
