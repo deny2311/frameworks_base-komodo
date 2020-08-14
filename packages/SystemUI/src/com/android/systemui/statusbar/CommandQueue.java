Project-Xtended/frameworks_base

base: API to restart SystemUI without root
Change-Id: I4ce4762bee82d40daf49bacc846786bc9a3d938f
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -93,6 +93,7 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_SHOW_CHARGING_ANIMATION       = 44 << MSG_SHIFT;
     private static final int MSG_SHOW_PINNING_TOAST_ENTER_EXIT = 45 << MSG_SHIFT;
     private static final int MSG_SHOW_PINNING_TOAST_ESCAPE     = 46 << MSG_SHIFT;
+    private static final int MSG_RESTART_UI                    = 47 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -165,6 +166,7 @@ public class CommandQueue extends IStatusBar.Stub {
         default void onFingerprintHelp(String message) { }
         default void onFingerprintError(String error) { }
         default void hideFingerprintDialog() { }
+        default void restartUI() { }
     }
 
     @VisibleForTesting
@@ -551,6 +553,13 @@ public class CommandQueue extends IStatusBar.Stub {
         }
     }
 
+    public void restartUI() {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_RESTART_UI);
+            mHandler.obtainMessage(MSG_RESTART_UI).sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -795,6 +804,11 @@ public class CommandQueue extends IStatusBar.Stub {
                 case MSG_SHOW_PINNING_TOAST_ESCAPE:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).showPinningEscapeToast();
+                    }
+                    break;
+                case MSG_RESTART_UI:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).restartUI();
                     }
                     break;
             }

deny2311/frameworks_base-komodo

Signed-off-by: Kry9toN <dhimasbagusprayoga@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -119,6 +119,7 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
     private static final int MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW = 48 << MSG_SHIFT;
     private static final int MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW = 49 << MSG_SHIFT;
     private static final int MSG_TOGGLE_CAMERA_FLASH           = 90 << MSG_SHIFT;
+    private static final int MSG_RESTART_UI                    = 51 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -282,6 +283,7 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
         default void hideBiometricDialog() { }
         default void showInDisplayFingerprintView() { }
         default void hideInDisplayFingerprintView() { }
+	default void restartUI() { }
 
         /**
          * @see IStatusBar#onDisplayReady(int)
@@ -855,6 +857,14 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
         }
     }
 
+    @Override
+    public void toggleCameraFlash() {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_TOGGLE_CAMERA_FLASH);
+            mHandler.sendEmptyMessage(MSG_TOGGLE_CAMERA_FLASH);
+        }
+    }
+
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -1118,6 +1128,11 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
                 case MSG_RECENTS_ANIMATION_STATE_CHANGED:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).onRecentsAnimationStateChanged(msg.arg1 > 0);
+		    }
+		    break;
+                case MSG_RESTART_UI:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).restartUI();
                     }
                     break;
                 case MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW: