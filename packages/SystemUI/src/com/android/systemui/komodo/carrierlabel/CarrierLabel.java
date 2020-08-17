DirtyUnicorns/android_frameworks_base

CarrierLabel: Notch friendly carrier label [1/2]
- Remove unnecessary imports
- Make sure it survives a reboot using notch settings

Change-Id: I4da7f7420a496ab26eb514a692b1d91713e3743e

packages/SystemUI/src/com/android/systemui/du/carrierlabel/CarrierLabel.java
@@ -17,11 +17,9 @@
 package com.android.systemui.du.carrierlabel;
 
 import android.content.BroadcastReceiver;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.database.ContentObserver;
 import android.graphics.Rect;
 import android.os.Handler;
 import android.os.UserHandle;
@@ -29,8 +27,6 @@ import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
 import com.android.internal.telephony.TelephonyIntents;
@@ -41,10 +37,6 @@ import com.android.systemui.Dependency;
 import com.android.systemui.du.carrierlabel.SpnOverride;
 import com.android.systemui.plugins.DarkIconDispatcher;
 import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.TimeZone;
 
 public class CarrierLabel extends TextView implements DarkReceiver {
 
@@ -64,6 +56,30 @@ public class CarrierLabel extends TextView implements DarkReceiver {
         super(context, attrs, defStyle);
         mContext = context;
         updateNetworkName(true, null, false, null);
+
+        int mCarrierLabel = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_CARRIER, 1, UserHandle.USER_CURRENT);
+        if (Utils.hasNotch(mContext)) {
+            switch (mCarrierLabel) {
+                case 0:
+                    setCarrierLabel("0");
+                    break;
+                case 1:
+                    setCarrierLabel("1");
+                    break;
+                case 2:
+                    setCarrierLabel("1");
+                    break;
+                case 3:
+                    setCarrierLabel("1");
+                    break;
+            }
+        }
+    }
+
+    private void setCarrierLabel(String value) {
+        Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_CARRIER, Integer.parseInt(value));
     }
 
     @Override

deny2311/frameworks_base-komodo

Signed-off-by: Kry9toN <dhimasbagusprayoga@gmail.com>

packages/SystemUI/src/com/android/systemui/komodo/carrierlabel/CarrierLabel.java
@@ -17,19 +17,16 @@
 package com.android.systemui.komodo.carrierlabel;
 
 import android.content.BroadcastReceiver;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.database.ContentObserver;
+import android.graphics.Rect;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
 import com.android.internal.telephony.TelephonyIntents;
@@ -38,11 +35,7 @@ import com.android.internal.util.komodo.KomodoUtils;
 import com.android.systemui.R;
 import com.android.systemui.komodo.carrierlabel.SpnOverride;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.TimeZone;
-
-public class CarrierLabel extends TextView {
+public class CarrierLabel extends TextView implements DarkReceiver {
 
     private Context mContext;
     private boolean mAttached;
@@ -60,6 +53,30 @@ public class CarrierLabel extends TextView {
         super(context, attrs, defStyle);
         mContext = context;
         updateNetworkName(true, null, false, null);
+
+        int mCarrierLabel = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_CARRIER, 1, UserHandle.USER_CURRENT);
+        if (aosipUtils.hasNotch(mContext)) {
+            switch (mCarrierLabel) {
+                case 0:
+                    setCarrierLabel("0");
+                    break;
+                case 1:
+                    setCarrierLabel("1");
+                    break;
+                case 2:
+                    setCarrierLabel("1");
+                    break;
+                case 3:
+                    setCarrierLabel("1");
+                    break;
+            }
+        }
+    }
+
+    private void setCarrierLabel(String value) {
+        Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_CARRIER, Integer.parseInt(value));
     }
 
     @Override