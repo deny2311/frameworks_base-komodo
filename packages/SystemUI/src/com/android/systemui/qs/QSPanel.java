1c8e86d2bf48e8ccf6aef3eedbf94c6bb2daef7b
Lucchetto <lucchetto.tie@live.com>
2020-09-25 16:12:50 +0200

    SystemUI: draw qs panel without side margins
        * Fix animation beings clipped in the container
        * partially reverts behaviour before 35484aed51cd7947938d5879e16c2595d2f7ee5f
        * but preverse the new margins logic
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: Ic9b29764e4d7177ab75eff5bc9cc415feaf55590

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -1106,6 +1106,10 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         }
     }
 
+    int getVisualTilePadding() {
+        return mVisualTilePadding;
+    }
+
     public void setContentMargins(int startMargin, int endMargin) {
         // Only some views actually want this content padding, others want to go all the way
         // to the edge like the brightness slider
@@ -1149,11 +1153,11 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     }
 
     private void updateTileLayoutMargins() {
-        int marginEnd = mVisualMarginEnd;
         if (mUsingHorizontalLayout) {
-            marginEnd = 0;
+            mTileLayout.setSidePadding(0, 0);
+        } else {
+            mTileLayout.setSidePadding(mVisualMarginStart, mVisualMarginEnd);
         }
-        updateMargins((View) mTileLayout, mVisualMarginStart, marginEnd);
     }
 
     /**
@@ -1252,6 +1256,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         int getOffsetTop(TileRecord tile);
 
         boolean updateResources();
+
+        void setSidePadding(int paddingStart, int paddingEnd);
 
         void setListening(boolean listening);
 
