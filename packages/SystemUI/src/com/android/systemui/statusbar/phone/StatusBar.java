DirtyUnicorns/android_frameworks_base

Traffic indicators: don't show two instances in expanded statusbar
we keep the one on top right of the screen (there we have more space
and in the future can add more net traffic info)

Change-Id: I098c93b9dd0edd146d08fd23c8c721274b9859ec

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1644,6 +1644,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (!isExpanded) {
             mRemoteInputManager.onPanelCollapsed();
         }
+        ((StatusBarIconControllerImpl) mIconController).onPanelExpanded(isExpanded);
     }
 
     public ViewGroup getNotificationScrollLayout() {
@@ -3673,6 +3674,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         updateScrimController();
         mPresenter.updateMediaMetaData(false, mState != StatusBarState.KEYGUARD);
         updateKeyguardState();
+    
+        ((StatusBarIconControllerImpl) mIconController).setKeyguardShowing(mState == StatusBarState.KEYGUARD);
         Trace.endSection();
     }
 

deny2311/frameworks_base-komodo

Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>
Signed-off-by: alanndz <alanndz7@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1865,6 +1865,7 @@ public class StatusBar extends SystemUI implements
         if (!isExpanded) {
             mRemoteInputManager.onPanelCollapsed();
         }
+        ((StatusBarIconControllerImpl) mIconController).onPanelExpanded(isExpanded);
     }
 
     public ViewGroup getNotificationScrollLayout() {
@@ -4408,6 +4409,8 @@ public class StatusBar extends SystemUI implements
                     updateScrimController();
                     mPresenter.updateMediaMetaData(false, mState != StatusBarState.KEYGUARD);
                     updateKeyguardState();
+
+                    ((StatusBarIconControllerImpl) mStatusBarIconController).setKeyguardShowing(mState == StatusBarState.KEYGUARD);
                     Trace.endSection();
                 }
 